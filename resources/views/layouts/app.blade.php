bdbf18351e4d2326fa34972f1533a1c55c03f08b
desirerd <desirerd@gmail.com>
2022-11-06 01:11:27 +0100

    cambios en appblade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,14 @@
         <!-- Fonts 
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         -->
-
+ 
     </head>
     <body class="bg-gray-100">
         <header class="p-5 border-b bg-white shadow">
           <div class="container mx-auto flex justify-between">
                  <h1 class="text-3xl font-black"> Devstagram </h1>
           </div>
-
+          <p> Nuevos cambios en el proyecto de Laravel </p>
           <nav>
                 <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
                 <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta </a>
